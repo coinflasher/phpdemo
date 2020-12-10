d5dc5b984304d0e4e3323feab593c760a1ff905e
Chad Harrelson <coinflasher@gmail.com>
2020-12-09 21:00:22 -0500

    ver3

--- index.php
+++ index.php
@@ -4,18 +4,32 @@ $myhostname = gethostname();
 
 ?>
 
-<TABLE width=900 cellspacing=0 cellpadding=0>
+<TABLE width=100% cellspacing=0 cellpadding=0>
 <TR>
   <TD>
-    <table width=900 bgcolor=black>
+    <table width=100% bgcolor=black>
       <tr>
-        <td width=100>&nbsp;</td>
+        <td width=50>&nbsp;</td>
         <td align=left><img src=rhlogo.png></td>
         <td align=center><b><font face="arial" color="white">My Custom PHP App</font></b></td>
-        <td align=right><font face="arial" color="white">Ver: <?php print "$ver"; ?></font></td>
-        <td width=100>&nbsp;</td>
+        <td align=right><font size="2" face="arial" color="white">Ver: <?php print "$ver"; ?></font></td>
+        <td width=50>&nbsp;</td>
       </tr>
     </table>
   </TD>
 </TR>
 </TABLE>
+<TABLE width=100% cellspacing=0 cellpadding=0>
+<TR>
+  <TD>
+    <table width=100% bgcolor=white>
+      <tr>
+        <td width=50>&nbsp;</td>
+        <td align=center><font size="+3" face="arial" color="white">Hostname: <?php print "$myhostname"; ?></font></b></td>
+        <td width=50>&nbsp;</td>
+      </tr>
+    </table>
+  </TD>
+</TR>
+</TABLE>
+
