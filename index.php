5592e9ce6600532bd0e0f1e773a52eecaed96068
Chad Harrelson <coinflasher@gmail.com>
2020-12-09 20:43:35 -0500

    update 1

--- index.php
+++ index.php
@@ -3,3 +3,16 @@ $ver = '3.5';
 $myhostname = gethostname();
 
 print "HOSTNAME: $myhostname | VER: $ver\n";
+?>
+
+<TABLE width=900 cellspacing=0 cellpadding=0>
+<TR>
+  <TD>
+    <table width=900 bgcolor=black>
+      <tr>
+        <td><font color="white">Version</font></td>
+      </tr>
+    </table>
+  </TD>
+</TR>
+</TABLE>
