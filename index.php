ceff6dc724b5791d37396dbdd658f3d555f612d6
Chad Harrelson <coinflasher@gmail.com>
2020-12-15 08:48:42 -0500

    V2

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $bgcolor = getenv('BGCOLOR');
     <table width=100% bgcolor="#2d1c46" border=0>
       <tr>
         <td width=50>&nbsp;</td>
-        <td align=left width=100><img src=truistlogo.jpg></td>
+        <td align=left width=100><img src=logo.jpg></td>
         <td align=center><b><font face="arial" color="white">-={ My Custom PHP App }=-</font></b></td>
         <td align=right width=100><font size="3" face="arial" color="white">
           Ver: <?php print "$ver"; ?>
