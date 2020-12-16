66d9785bd246a40b0086c3682f45e4ccbcba4165
Chad Harrelson <coinflasher@gmail.com>
2020-12-16 10:08:26 -0500

    V2

--- index.php
+++ index.php
@@ -1,16 +1,15 @@
 <?php
 $ver = '2.0';
 $myhostname = gethostname();
-$bgcolor = getenv('BGCOLOR');
 ?>
 
 <TABLE width=100% cellspacing=0 cellpadding=0>
 <TR>
   <TD>
-    <table width=100% bgcolor="#2d1c46" border=0>
+    <table width=100% bgcolor="black" border=0>
       <tr>
         <td width=50>&nbsp;</td>
-        <td align=left width=100><img src=logo.jpg></td>
+        <td align=left width=100><img src=rhlogo.png></td>
         <td align=center><b><font face="arial" color="white">-={ My Custom PHP App }=-</font></b></td>
         <td align=right width=100><font size="3" face="arial" color="white">
           Ver: <?php print "$ver"; ?>
@@ -23,7 +22,7 @@ $bgcolor = getenv('BGCOLOR');
 </TR>
 </TABLE>
 <BR><BR><BR>
-<TABLE width=100% cellspacing=0 cellpadding=0 bgcolor="<?php print "$bgcolor";?>">
+<TABLE width=100% cellspacing=0 cellpadding=0>
 <TR>
   <TD>
     <table width=100%>
